b18f5ad710c1d8d10bf0726ee17db4e7a07a42ad
m1racul <nnahid929@gmail.com>
2022-01-05 12:19:32 +0600

    rps

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -3,9 +3,56 @@ import random
 rps = ["paper","rock","scissor"]
 
 
-rps1 = input("rock,paper,scissor:")
+rps1 = ""
 
-rand_rps = random.choice(rps)
+point = 0
 
 
-print(rand_rps)
+while True:
+    rps1 = input("rock,paper,scissor:")
+
+    
+
+    rand_rps = random.choice(rps)
+
+    print(rand_rps)
+
+    if rps1==rand_rps:
+        print("draw")
+        print(point)
+
+    elif rps1=="rock" and rand_rps=="scissor":
+        print("win")
+        point = point + 1
+        print(point)
+
+    elif rps1=="rock" and rand_rps=="paper":
+        print("lost")
+        point = point - 1
+        print(point)
+
+    elif rps1=="paper" and rand_rps=="scissor":
+        print("lost")
+        point = point - 1
+        print(point)
+
+    elif rps1=="paper" and rand_rps=="rock":
+        print("win")
+        point = point + 1
+        print(point)
+
+    elif rps1=="scissor" and rand_rps=="paper":
+        print("win")
+        point = point + 1
+        print(point)
+
+    elif rps1=="scissor" and rand_rps=="rock":
+        print("lost")
+        point = point - 1
+        print(point)
+
+    elif rps1=="exit" or rps1=="quit":
+        break
+
+    else:
+        print("BLA")
